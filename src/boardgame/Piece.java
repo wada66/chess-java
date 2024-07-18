b602f6655b5ad2cce21d3168ca39a2ade8cab325
wada <felipewada@hotmail.com>
2024-07-18 19:13:58 -0300

    imprimindo tabuleiro.

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -5,4 +5,21 @@ public class Piece {
 	protected Position position;
 	private Board board;
 	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
